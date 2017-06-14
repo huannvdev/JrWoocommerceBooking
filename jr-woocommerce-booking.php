d80ab0200fcd1b933295bf938785b0f61bf9c036
Jream <huannv.dev@gmail.com>
2017-06-14 08:33:39 +0700

    Add core plugin

---
+++ jr-woocommerce-booking.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: Jr Woocommerce Booking
+ * Plugin URI: http://kamixkei.com
+ * Description: Booking extension for woocommerce plugin
+ * Version: 1.0
+ * Author: Jream
+ * Author URI: http://kamixkei.com
+ * Text Domain: jr-woocommerce-booking
+ * Domain Path: languages/
+ */
